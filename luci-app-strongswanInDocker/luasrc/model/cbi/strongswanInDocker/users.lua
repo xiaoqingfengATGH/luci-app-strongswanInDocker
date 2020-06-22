b85d60d8d71a823dc4aea7c74e4d98b960dbffef
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-06-22 21:51:36 +0800

    Auto decompress config files at first launch

--- luci-app-strongswanInDocker/luasrc/model/cbi/strongswanInDocker/users.lua
+++ luci-app-strongswanInDocker/luasrc/model/cbi/strongswanInDocker/users.lua
@@ -1,6 +1,5 @@
-mp = Map("ipsec", translate("IPSec VPN Server"))
-mp.description = translate(
-                     "IPSec VPN connectivity using the native built-in VPN Client on iOS or Andriod (IKEv1 with PSK and Xauth)")
+mp = Map("ipsec", translate("IPSec VPN Server(Docker)"))
+mp.description = translate("IPSec VPN connectivity using the native built-in VPN Client on iOS or Andriod (IKEv1 with PSK and Xauth) & Windows 10 (IKEv2)")
 
 s = mp:section(TypedSection, "users", translate("Users Manager"))
 s.addremove = true
