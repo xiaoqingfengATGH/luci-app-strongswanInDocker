e1690b9a7f8a042c73a915ea4a1049d5c2abe471
Richard Yu <31612198+xiaoqingfengATGH@users.noreply.github.com>
2020-06-22 11:06:53 +0800

    Delete PID file in case of download abnormal exit

--- luci-app-strongswanInDocker/root/etc/strongswanInDocker/doDownload.sh
+++ luci-app-strongswanInDocker/root/etc/strongswanInDocker/doDownload.sh
@@ -21,15 +21,17 @@ echo == M >> $DOWNLOAD_PROGRESS
 downloadedMd5=$(md5sum $DOWNLOAD_FILE | cut -d  ' ' -f 1)
 if [ "$downloadedMd5" != "$DOCKER_IMAGE_MD5" ]; then
 	echo 下载文件完整性校验失败! >> $DOWNLOAD_PROGRESS
-#	rm -rf /tmp/strongswanInDocker
+	rm -f $DOWNLOAD_PID
+	rm -rf $DOWNLOAD_FILE
 	exit 1
 fi
 echo == MS >> $DOWNLOAD_PROGRESS
 
 echo == U >> $DOWNLOAD_PROGRESS
-7z x -so /tmp/strongswanInDocker/xiaoqingfeng999-strongswan-5.8.4.7z 2>>$DOWNLOAD_PROGRESS |docker load >> $DOWNLOAD_PROGRESS 2>&1
+7z x -so $DOWNLOAD_FILE 2>>$DOWNLOAD_PROGRESS |docker load >> $DOWNLOAD_PROGRESS 2>&1
 if [ $? -ne 0 ]; then
 	echo 解压并载入Docker镜像失败! >> $DOWNLOAD_PROGRESS
+	rm -f $DOWNLOAD_PID
 	exit 2
 fi
 echo == US >> $DOWNLOAD_PROGRESS
@@ -38,6 +40,7 @@ echo == I >> $DOWNLOAD_PROGRESS
 docker tag $DOCKER_IMAGE_ID xiaoqingfeng999/strongswan:5.8.4 >> $DOWNLOAD_PROGRESS 2>&1
 if [ $? -ne 0 ]; then
 	echo Tag Docker镜像失败! >> $DOWNLOAD_PROGRESS
+	rm -f $DOWNLOAD_PID
 	exit 3
 fi
 echo == IS >> $DOWNLOAD_PROGRESS
